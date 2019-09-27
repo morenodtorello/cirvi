9703c782d336d1d826dbe9a86a95bee502a30deb
morenodtorello <morenodtorello@gmail.com>
2019-09-27 17:30:04 +0200

    cat

--- suma.java
+++ suma.java
@@ -1,8 +1,8 @@
 int numero1 = 0;
 int numero2 = 0;
 
-System.out.println("Introdueix el primer nombre");
+System.out.println("Introdueix el primer nombre:");
 numero1 = reader.nextInt();
 
-System.out.println("Introdueix el segon nombre");
+System.out.println("Introdueix el segon nombre:");
 numero2 = reader.nextInt();
