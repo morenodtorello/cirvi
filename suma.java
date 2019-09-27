94d18c370720b490c8a78b82e49671827cb26623
morenodtorello <morenodtorello@gmail.com>
2019-09-27 17:26:18 +0200

    Catala

--- suma.java
+++ suma.java
@@ -1,8 +1,8 @@
 int numero1 = 0;
 int numero2 = 0;
 
-System.out.println("Introduce the first number");
+System.out.println("Introdueix el primer nombre");
 numero1 = reader.nextInt();
 
-System.out.println("Introduce the second number");
+System.out.println("Introdueix el segon nombre");
 numero2 = reader.nextInt();
